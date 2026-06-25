1d65f2f6eecaf0dc47a2aa05bf1fe8a2a53b29ce
kr1shnav <krishnavbarman24@gmail.com>
2026-06-25 21:07:05 +0530

    Status

---
+++ repowise/commands/status.py
@@ -0,0 +1,27 @@
+from git import Repo
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+def status():
+    repo = Repo(".", search_parent_directories=True)
+
+    table = Table(title="Repository Status")
+
+    table.add_column("Type",style="cyan")
+    table.add_column("File", style="green")
+
+    # Untracked files
+    for file in repo.untracked_files:
+        table.add_row("Untracked", file)
+
+    # Modified files
+    for item in repo.index.diff(None):
+        table.add_row("Modified", item.a_path)
+
+    # Staged files
+    for item in repo.index.diff("HEAD"):
+        table.add_row("Staged", item.a_path)
+
+    console.print(table)
--- repowise/main.py
+++ repowise/main.py
@@ -1,5 +1,7 @@
 import typer
+
 from commands.summary import summary
+from commands.status import status
 
 app = typer.Typer()
 
@@ -8,7 +10,9 @@ app = typer.Typer()
 def version():
     print("Repowise v0.1")
 
+
 app.command()(summary)
+app.command()(status)
 
 
 if __name__ == "__main__":
